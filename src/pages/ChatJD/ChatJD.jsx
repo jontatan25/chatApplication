7d782faed63839f9aeac5ab54aa5c28877aaa540
Jonathan Sarmiento <jhonn.photo@gmail.com>
2022-11-23 04:26:38 -0500

    Style: Online users presentational container implemented

--- src/pages/ChatJD/ChatJD.jsx
+++ src/pages/ChatJD/ChatJD.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import Chat from '../../components/Chat/Chat'
-import "./style.css"
+import React from "react";
+import Chat from "../../components/Chat/Chat";
+import "./style.css";
 
-
+import usersImg from "../../img/people-icon.png";
 const ChatJD = () => {
   return (
     <div className="chat__container -flex">
-
-        <div className="chat__window">
-            <div className="chat__window__title -title -flex">Chat</div>
-          <Chat/>
+      <div className="chat__window">
+        <div className="chat__window__title -title -flex">Chat</div>
+        <Chat />
+      </div>
+      <div className="chat__users">
+        <div className="chat__window__title -users-title -title -flex">
+          <img
+            className="chat__users__img"
+            src={usersImg}
+            alt="group of people chatting"
+          />{" "}
+          Users Online &#10088;#&#10089;
         </div>
-        <div className="chat__users"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatJD
+export default ChatJD;
